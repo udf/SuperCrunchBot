9b2268a16ce68ade1b47cdb72907b2cf08a0454e
udf <tabhooked@gmail.com>
2019-04-29 22:03:13 +0200

    add shell

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+    python = pkgs.python37.override {
+        packageOverrides = self: super: {
+            imagemagick7Big = pkgs.imagemagick7Big.overrideAttrs (oldAttrs: rec {
+                buildInputs = oldAttrs.buildInputs ++ [ pkgs.liblqr1 ];
+            });
+        };
+    };
+    python-pkgs = python.withPackages (ps: [
+        ps.Wand ps.telethon
+    ]);
+in pkgs.mkShell {
+    name = "super-crunch-bot-shell";
+    buildInputs = [ python-pkgs ];
+}
